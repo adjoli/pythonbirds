2e32fb9f563831e477b4c24b1461f76d18a59d5f
Adao Oliveira <adaum79@gmail.com>
2020-05-26 16:15:18 -0300

    Criados os atributos de instância 'nome' e 'idade' Criado o método __repr__

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,10 +1,21 @@
 class Pessoa:
+    def __init__(self, nome=None, idade=41):
+        self.nome = nome
+        self.idade = idade
+
     def cumprimentar(self):
-        return f'Ola {id(self)}'
+        return f"Ola {id(self)}"
+
+    def __repr__(self):
+        return f"{self.nome} tem {self.idade} anos"
 
 
 if __name__ == '__main__':
-    p = Pessoa()
-    print(Pessoa.cumprimentar(p))
-    print(p.cumprimentar())
-    print(id(p))
+    p1 = Pessoa('Xico')
+    p2 = Pessoa('Arthur', 7)
+    print(Pessoa.cumprimentar(p1))
+    print(p1.cumprimentar())
+    print(id(p1))
+
+    print(p1)
+    print(p2)
